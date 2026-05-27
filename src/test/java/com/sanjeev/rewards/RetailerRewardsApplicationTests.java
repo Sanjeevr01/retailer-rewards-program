8767f2a92116f6f503f6e7216ed302397a4a6d03
Sanjeev R <sanjeevr@Sanjeevs-MacBook-Air.local>
2026-05-27 18:46:25 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/sanjeev/rewards/RetailerRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjeev.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailerRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
